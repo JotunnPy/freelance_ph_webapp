9c231f9d9055e64a4dd64945cb54dbaec34bbd3b
JotunnPy <106374742+JotunnPy@users.noreply.github.com>
2023-03-21 12:29:50 -0400

    Initial Commit

---
+++ firebase/firebase.ts
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import "firebase/auth";
+import { getAuth } from "firebase/auth";
+import "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDWxFsr3uJTGbe1PDXGTEVQjNtiuxUXQLU",
+  authDomain: "freelance-ph-caf13.firebaseapp.com",
+  projectId: "freelance-ph-caf13",
+  storageBucket: "freelance-ph-caf13.appspot.com",
+  messagingSenderId: "469917388345",
+  appId: "1:469917388345:web:4f3634f9d52e959d24c7e8",
+  measurementId: "G-Y21V1GPEZE",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
---
+++ pages/chicken.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Chickn() {
+  return <div>Chicken</div>;
+}
+
+export default Chickn;
