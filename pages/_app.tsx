33d1cb8f5c8be489f42607af31d9bc428504c8f5
JotunnPy <106374742+JotunnPy@users.noreply.github.com>
2023-03-24 22:35:09 -0400

    Reset

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,6 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <div>
-      <text>Chansen is big cock</text>
-      <Component {...pageProps} />
-    </div>
-  );
+  return <Component {...pageProps} />
 }
--- pages/chicken.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Chickn() {
-  return <div>Chicken</div>;
-}
-
-export default Chickn;
